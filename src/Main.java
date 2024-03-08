9d86216c02e981ca16b02fac43927d8b48556e6f
Baraa Mohamed <albaraa.m.m2005@gmail.com>
2024-03-08 06:28:17 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,9 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
+        for (int i = 0; i <100; i++) {
         System.out.println(Person.fake());
+        }
     }
-
-
 }
+
--- src/models/Person.java
+++ src/models/Person.java
@@ -9,6 +9,16 @@ public class Person {
     private float weight;
 
     private float height;
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    private Gender gender;
 //    public Person() {
 //        this.name = name;
 //        this.age = age;
@@ -57,6 +67,7 @@ public class Person {
                 ", age=" + age +
                 ", weight=" + weight +
                 ", height=" + height +
+                ", gender=" + gender +
                 '}';
     }
 
@@ -67,13 +78,18 @@ public class Person {
         int fakeAge = Math.abs(random.nextInt()) % 80;
         int fakeWeight = 40 + Math.abs(random.nextInt()) % 100;
         int fakeHeight = 100 + Math.abs(random.nextInt()) % 100;
+        Gender fakeGender =  Math.abs(random.nextInt()) % 2 == 0? Gender.Male: Gender.Female;
 
         Person person = new Person();
         person.setName(fakeName);
         person.setAge(fakeAge);
         person.setWeight(fakeWeight);
         person.setHeight(fakeHeight);
+        person.setGender(fakeGender);
 
         return person.toString();
     }
+}
+enum Gender {
+    Male, Female;
 }
